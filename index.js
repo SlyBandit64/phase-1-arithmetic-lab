c16b6daaba1ea77cc9272ba8e79ee77f50d5a6c9
Scott Taylor <rscott.taylor01@gmail.com>
2023-04-28 21:14:51 -0500

    Copmletes lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 100);
+
+let num3 = 14;
+let num4 = 5;
+const mod = num3 % num4;
+
+let numberSet = [10, 20, 15, 5, 8];
+const max = Math.max(...numberSet);
+
+
